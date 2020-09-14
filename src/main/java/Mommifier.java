f815bd04698cfea4b1f12bfd7f19457a810ee5ae
Lu <1625196521@qq.com>
2020-09-14 15:44:22 +0800

    is muber of vowels more than thirty percent in test file

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,11 @@
+public class Mommifier {
+    public Boolean mommyInsert(String str) {
+//        int j = 0;
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+//                j += 1;
+//            }
+//        }
+        return  true;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MommifierTest {
+
+    @Test
+    void is_number_of_vowels_more_than_thirty_percent() {
+        Mommifier mommy = new Mommifier();
+        String str1 = "aaaaaeeeeiisfddvr";
+        Boolean bln = mommy.mommyInsert(str1);
+        assertEquals(true,bln);
+    }
+
 }
